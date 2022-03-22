5c961cefeb7b3de198743e17c02e6944f232f500
Santosh Kumar Nayak <santonayak@deloitte.com>
2022-03-22 14:07:02 +0530

    JPA and Postgress driver added

--- src/main/java/com/hashedin/NetflixBingeWatchRelationalDB/NetflixBingeWatchRelationalDbApplication.java
+++ src/main/java/com/hashedin/NetflixBingeWatchRelationalDB/NetflixBingeWatchRelationalDbApplication.java
@@ -1,13 +1,26 @@
 package com.hashedin.NetflixBingeWatchRelationalDB;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NetflixBingeWatchRelationalDbApplication {
 
+	@Autowired
+	TestRepository testRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixBingeWatchRelationalDbApplication.class, args);
+	}
+
+	@PostMapping("/create")
+	public Test createTest(@RequestBody Test test){
+		return  testRepository.save(test);
+
 	}
 
 }
